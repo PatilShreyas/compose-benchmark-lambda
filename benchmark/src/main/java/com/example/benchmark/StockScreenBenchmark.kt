9f77fe84521a7dd11f38c6d63ca8d7dc7d8a8c7d
Shreyas Patil <shreyaspatilg@gmail.com>
2024-11-17 16:28:52 +0530

    Rename file

--- benchmark/src/main/java/com/example/benchmark/StockScreenBenchmark.kt
+++ benchmark/src/main/java/com/example/benchmark/StockScreenBenchmark.kt
@@ -6,11 +6,8 @@ import androidx.benchmark.macro.FrameTimingMetric
 import androidx.benchmark.macro.MemoryCountersMetric
 import androidx.benchmark.macro.MemoryUsageMetric
 import androidx.benchmark.macro.StartupMode
-import androidx.benchmark.macro.StartupTimingMetric
 import androidx.benchmark.macro.junit4.MacrobenchmarkRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.uiautomator.By
-import androidx.test.uiautomator.Direction
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
